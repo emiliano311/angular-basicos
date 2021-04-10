ed60207931ece6f23ab51c9bdb51e334ebb627fc
Emiliano <emiliano.salorio@gmail.com>
2021-04-10 18:53:33 -0300

    usando servicios para los datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModulo } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DgzModule } from './dbz/dgz.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     HeroesModulo,
-    ContadorModule
+    ContadorModule,
+    DgzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
