ab5e7736b9da96b0840924a39657db378433c89b
Emiliano <emiliano.salorio@gmail.com>
2021-06-25 07:35:15 -0300

    terminado

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { Personaje } from '../interfaces/dbz.interface';
+import { Component} from '@angular/core';
 import { DbzService } from '../services/dbz.service';
 
 @Component({
@@ -9,7 +8,7 @@ import { DbzService } from '../services/dbz.service';
 export class PersonajesComponent {
 
   // @Input('data') personajes: Personaje[] =[];
-  
+
   get Personaje(){
    return this.DbzService.personajes; 
   }
